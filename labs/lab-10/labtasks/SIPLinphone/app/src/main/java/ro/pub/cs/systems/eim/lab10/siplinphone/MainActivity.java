782ff363a6a34821bac1304f9340de9d5dd29304
Mihai Pacuraru <mihaidorin653@gmail.com>
2026-01-21 17:01:37 +0200

    Finish lab 10

--- labs/lab-10/labtasks/SIPLinphone/app/src/main/java/ro/pub/cs/systems/eim/lab10/siplinphone/MainActivity.java
+++ labs/lab-10/labtasks/SIPLinphone/app/src/main/java/ro/pub/cs/systems/eim/lab10/siplinphone/MainActivity.java
@@ -18,18 +18,84 @@ import org.linphone.core.Account;
 import org.linphone.core.AccountParams;
 import org.linphone.core.Address;
 import org.linphone.core.AuthInfo;
+import org.linphone.core.Call;
+import org.linphone.core.CallParams;
 import org.linphone.core.Core;
+import org.linphone.core.CoreListener;
+import org.linphone.core.CoreListenerStub;
 import org.linphone.core.Factory;
+import org.linphone.core.MediaEncryption;
+import org.linphone.core.RegistrationState;
 import org.linphone.core.TransportType;
 
 public class MainActivity extends AppCompatActivity {
+    private Core core;
+
+    private final CoreListener coreListener = new CoreListenerStub() {
+        @Override
+        public void onAccountRegistrationStateChanged(Core core, Account account, RegistrationState state, String message) {
+            ((TextView) findViewById(R.id.registration_status)).setText(message);
+
+            if (state != null) {
+                switch (state) {
+                    case Failed:
+                        findViewById(R.id.register).setEnabled(true);
+                        break;
+                    case Cleared:
+                        findViewById(R.id.register_layout).setVisibility(View.VISIBLE);
+                        findViewById(R.id.call_layout).setVisibility(View.GONE);
+                        findViewById(R.id.register).setEnabled(true);
+                        break;
+                    case Ok:
+                        findViewById(R.id.register_layout).setVisibility(View.GONE);
+                        findViewById(R.id.call_layout).setVisibility(View.VISIBLE);
+                        findViewById(R.id.unregister).setEnabled(true);
+                        findViewById(R.id.remote_address).setEnabled(true);
+                        break;
+                }
+            }
+        }
+
+        @Override
+        public void onCallStateChanged(Core core, Call call, Call.State state, String message) {
+            ((TextView) findViewById(R.id.call_status)).setText(message);
+
+            if (state != null) {
+                switch (state) {
+                    case IncomingReceived:
+                        findViewById(R.id.hang_up).setEnabled(true);
+                        findViewById(R.id.answer).setEnabled(true);
+
+                        String remoteAddress = call.getRemoteAddressAsString();
+                        if (remoteAddress != null) {
+                            ((EditText) findViewById(R.id.remote_address)).setText(remoteAddress);
+                        }
+                        break;
+                    case Connected:
+                        findViewById(R.id.mute_mic).setEnabled(true);
+                        findViewById(R.id.toggle_speaker).setEnabled(true);
+                        Toast.makeText(MainActivity.this, "Remote party answered", Toast.LENGTH_LONG).show();
+                        break;
+                    case Released:
+                        findViewById(R.id.hang_up).setEnabled(false);
+                        findViewById(R.id.answer).setEnabled(false);
+                        findViewById(R.id.mute_mic).setEnabled(false);
+                        findViewById(R.id.toggle_speaker).setEnabled(false);
+                        ((EditText) findViewById(R.id.remote_address)).getText().clear();
+                        findViewById(R.id.call).setEnabled(true);
+                        break;
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
         Factory factory = Factory.instance();
-        Core core = factory.createCore(null, null, this);
+        core = factory.createCore(null, null, this);
 
         TextView status = findViewById(R.id.registration_status);
         status.setText("linphone lib version: " + core.getVersion());
@@ -40,9 +106,11 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.register_layout).setVisibility(View.GONE);
                 findViewById(R.id.call_layout).setVisibility(View.VISIBLE);
 
-                if (login(core)) {
-
+                if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 0);
                 }
+
+                login();
             }
         });
 
@@ -51,11 +119,47 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 findViewById(R.id.register_layout).setVisibility(View.VISIBLE);
                 findViewById(R.id.call_layout).setVisibility(View.GONE);
+
+                Account account = core.getDefaultAccount();
+                if (account != null) {
+                    AccountParams params = account.getParams();
+                    AccountParams clonedParams = params.clone();
+                    clonedParams.setRegisterEnabled(false);
+                    account.setParams(clonedParams);
+                    v.setEnabled(false);
+                }
             }
         });
+
+        findViewById(R.id.call).setOnClickListener(v -> {
+            outgoingCall();
+            findViewById(R.id.remote_address).setEnabled(false);
+            v.setEnabled(false);
+            findViewById(R.id.hang_up).setEnabled(true);
+        });
+
+        findViewById(R.id.hang_up).setOnClickListener(v -> {
+            findViewById(R.id.remote_address).setEnabled(true);
+            findViewById(R.id.call).setEnabled(true);
+
+            if (core.getCallsNb() != 0) {
+                Call call = core.getCurrentCall() != null ? core.getCurrentCall() : core.getCalls()[0];
+                if (call != null) {
+                    call.terminate();
+                }
+            }
+        });
+
+        findViewById(R.id.answer).setOnClickListener(v -> {
+            Call currentCall = core.getCurrentCall();
+            if (currentCall != null) {
+                currentCall.accept();
+            }
+        });
+
     }
 
-    private boolean login(Core core) {
+    private boolean login() {
         String username = ((EditText) findViewById(R.id.username)).getText().toString();
         String password = ((EditText) findViewById(R.id.password)).getText().toString();
         String domain = ((EditText) findViewById(R.id.domain)).getText().toString();
@@ -96,11 +200,23 @@ public class MainActivity extends AppCompatActivity {
 
         core.start();
 
-        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 0);
-            return false;
-        }
         return true;
+    }
+
+    private void outgoingCall() {
+        String remoteSipUri = ((EditText) findViewById(R.id.remote_address)).getText().toString();
+        Address remoteAddress = Factory.instance().createAddress("sip:" + remoteSipUri);
+        if (remoteAddress == null) {
+            return;
+        }
+
+        CallParams params = core.createCallParams(null);
+        if (params == null) {
+            return;
+        }
+
+        params.setMediaEncryption(MediaEncryption.None);
+        core.inviteAddressWithParams(remoteAddress, params);
     }
 
 }
